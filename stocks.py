8e4b247482b3df270538e00e2552b0c39177c46d
jkk004 <62976976+jkk004@users.noreply.github.com>
2020-05-06 23:05:50 -0700

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -4,18 +4,27 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
+plt.style.use('ggplot')
+fig = plt.figure(figsize = [20,9])
+print("Pick a starting year: ")
+first = input()
+print("Pick an ending year: ")
+second = input()
+print("Choose a stock (symbol not full name)")
+s1 = input()
+print("Choose another stock (symbol not full name)")
+s2 = input()
 style.use("ggplot")
-start = dt.datetime(2020,1,1)
-end = dt.datetime(2020,5,1)
+start = dt.datetime(int(first),1,1)
+end = dt.datetime(int(second),12,30)
 
-df = web.DataReader('aapl', 'yahoo', start,end)
-df["Adj Close"].plot(label='AAPL')
-df1 = web.DataReader('msft', 'yahoo', start,end)
-df1["Adj Close"].plot(label="MSFT")
+df = web.DataReader(s1, 'yahoo', start,end)
+df["Adj Close"].plot(label=s1.upper())
+df1 = web.DataReader(s2, 'yahoo', start,end)
+df1["Adj Close"].plot(label=s2.upper())
 df2 = (df1["Adj Close"] + df["Adj Close"])/2
 df2.plot(label = "Average")
 plt.legend()
-plt.title("TSLA VS MSFT")
+plt.title(s1.upper() + " VS " + s2.upper())
+
 plt.show()
-
-
